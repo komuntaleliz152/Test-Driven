4fa24dc347587c5083150f384140c8160c899e85
komuntale elizabeth <komuntaleliz152@gmail.com>
2025-07-07 16:26:49 +0300

    Added a test for multiply(4, 4) using assert.equal()

--- test/test.js
+++ test/test.js
@@ -18,3 +18,8 @@ it('should return 4 when multiplying 2 and 2', () => {
   it('should return 9 when multiplying 3 and 3', () => {
     assert.equal(multiply(3, 3), 9);
   });
+
+// Test: multiplying 4 by 4 should return 16
+  it('should return 16 when multiplying 4 and 4', () => {
+    assert.equal(multiply(4, 4), 16);
+  });
